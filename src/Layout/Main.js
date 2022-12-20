8b11b03b91139c685976c000b6433e01dbe76a18
developer-munir <devolopermunir@gmail.com>
2022-12-20 20:21:56 +0600

    setup layout

---
+++ src/Layout/Main.js
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import UserDetails from "../Components/UserDetails";
+import UserList from "../Components/UserList";
+import "./main.css";
+const Main = () => {
+  return (
+    <Grid container className="main-container">
+      <Grid item xs={12} sm={7} className="container-item">
+        <UserList></UserList>
+      </Grid>
+      <Grid item xs={12} sm={5} className="container-item">
+        <UserDetails></UserDetails>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Main;
