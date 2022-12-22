ef3f9e5d2344ccfc75ec0ac73ef043ed38ae3cb5
developer-munir <devolopermunir@gmail.com>
2022-12-22 15:00:30 +0600

    style details component

--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,9 +1,10 @@
-import { Avatar, Box, Stack } from "@mui/material";
+import { Avatar, Box, Stack, TextField } from "@mui/material";
 import React from "react";
+import "./component.css";
 
 const UserDetails = () => {
   return (
-    <div>
+    <Box>
       <Box
         sx={{
           backgroundColor: "black",
@@ -15,13 +16,56 @@ const UserDetails = () => {
         <h1>User Details</h1>
       </Box>
       <div>
-        <Box>
-          <Stack direction="row" spacing={2}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <Box sx={{ textAlign: "center", paddingTop: "5px" }}>
+          <Stack>
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+              sx={{ margin: "0 auto", width: 156, height: 156 }}
+            />
+            <h4>Munir Hossain Juwel</h4>
           </Stack>
         </Box>
+
+        <Box sx={{ padding: "20px" }}>
+          <Box
+            sx={{
+              border: "2px solid rgb(247, 247, 247)",
+              padding: "5px",
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            laboriosam quae ab blanditiis fugiat itaque atque possimus! Repellat
+            excepturi obcaecati ut vero aspernatur, voluptates blanditiis
+            perspiciatis quis fugit, reprehenderit id?
+          </Box>
+          <Box sx={{ marginTop: "10px", padding: "5px" }}>
+            <form className="form-style">
+              <label htmlFor="Full Name">Full Name</label>
+              <Box>
+                <input type="text" name="" id="" className="input-style" />
+              </Box>
+            </form>
+          </Box>
+          <Box sx={{ marginTop: "10px", padding: "5px" }}>
+            <form className="form-style">
+              <label htmlFor="Full Name">Job Title</label>
+              <Box>
+                <input type="text" name="" id="" className="input-style" />
+              </Box>
+            </form>
+          </Box>
+          <Box sx={{ marginTop: "10px", padding: "5px" }}>
+            <form className="form-style">
+              <label htmlFor="Full Name">Email</label>
+              <Box>
+                <input type="text" name="" id="" className="input-style" />
+              </Box>
+            </form>
+          </Box>
+        </Box>
       </div>
-    </div>
+    </Box>
   );
 };
 
